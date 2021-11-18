44f4ceb57a6a4656c323fc61488a8e6a6f7ce2e3
Regis Pires Magalhaes <regispiresmag@gmail.com>
2021-11-17 22:33:30 -0300

    melhorias em exemplo-dao-spring-jdbc

--- modulo02/exemplo-dao-spring-jdbc/src/main/java/br/ufc/quixada/entity/Cliente.java
+++ modulo02/exemplo-dao-spring-jdbc/src/main/java/br/ufc/quixada/entity/Cliente.java
@@ -1,66 +1,15 @@
 package br.ufc.quixada.entity;
 
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@ToString
 public class Cliente {
-	private int id;
-	private String cpf;
-	private String nome;
-	private String fone;
-	private double renda;
-
-	public Cliente() {}
-	
-	public Cliente(String cpf, String nome, String fone, double renda) {
-		this.cpf = cpf;
-		this.nome = nome;
-		this.fone = fone;
-		this.renda = renda;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getCpf() {
-		return cpf;
-	}
-
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getFone() {
-		return fone;
-	}
-
-	public void setFone(String fone) {
-		this.fone = fone;
-	}
-
-	public double getRenda() {
-		return renda;
-	}
-
-	public void setRenda(double renda) {
-		this.renda = renda;
-	}
-
-	@Override
-	public String toString() {
-		return "Cliente [id=" + id + ", cpf=" + cpf + ", nome=" + nome
-				+ ", fone=" + fone + ", renda=" + renda + "]";
-	}
-
-
+	@NonNull @Getter @Setter private int id;
+	@NonNull @Getter @Setter private String cpf;
+	@Getter @Setter private String nome;
+	@Getter @Setter private String fone;
+	@Getter @Setter private double renda;
 }
